4a6146d539201c44fa35671fbb2216ed95a82e0d
Bruno C <bruno.ferreira5@estudante.ufla.br>
2023-07-18 21:26:34 -0300

    Arrumando editar serviço

--- src/repositories/gerenciarVendaRestauracao.repository.js
+++ src/repositories/gerenciarVendaRestauracao.repository.js
@@ -252,13 +252,13 @@ async function deletarVendaRestauracao(inputId) {
 
 					console.log("Venda/Restauração removida com sucesso! Id da Venda/Restauração: " + inputId);
 
-					if(ehVenda == 0) {
+					if (ehVenda == 0) {
 						connection.query(sqlDeletarConsoleRestauracao, idConsole, function (err, resultDeletarConsoleRestauracao) {
 							if (err) {
 								console.log("Erro ao remover o console de restauração no banco de dados: ", err);
 								reject(err);
 							}
-	
+
 							console.log("Console de restauração removido com sucesso! Id do Console: " + idConsole);
 						})
 					}
@@ -304,13 +304,15 @@ async function consultaDeletarVendaRestauracaoRegiao(inputId, inputUser) {
 // Altera uma Venda/Restauração no Banco de Dados
 async function editarVendaRestauracao(dados, inputId) {
 
+	const sqlSelecionarDadosServicoAntigo = "SELECT FK_idEmpresa, FK_idConsole, qtdeConsoles FROM venda_restauracao WHERE idVenda_Restauracao=?"
 	const sqlSelecionarIdCliente = "SELECT idCliente FROM cliente WHERE cpfCliente = ?;";
 	const sqlSelecionarIdEmpresa = "SELECT idEmpresa FROM empresa WHERE estado = ?;";
 	const sqlSelecionarQuantAtual = "SELECT quantAtual FROM estoque WHERE FK_idConsole = ? AND FK_idEmpresa = ?;";
+	const sqlArrumarEstoque = "UPDATE estoque SET quantAtual = quantAtual + ? WHERE FK_idConsole = ? AND FK_idEmpresa = ?;";
 	const sqlAtualizarEstoque = "UPDATE estoque SET quantAtual = quantAtual - ? WHERE FK_idConsole = ? AND FK_idEmpresa = ?;";
 	const sqlDeletarEstoque = "DELETE FROM estoque WHERE FK_idConsole = ? AND FK_idEmpresa = ?;";
 	const sqlEditarTabelaConsole = "UPDATE console SET nomeConsole = ?, nomeFabricante = ?, dataLancamento = ?, ehOriginal = ?, preco = ?, descricaoConsole = ? WHERE idConsole = ?;"
-	const sqlEditarVendaRestauracao = "UPDATE venda_restauracao SET dataServico = ?, horaServico = ?, valorTotal = ?, ehVenda = ?, estaEntregue = ?, qtdeConsoles = ?, descricaoRestauracao = ?, avaliacao = NULL, FK_idEmpresa = ?, FK_idCliente = ?, FK_idConsole = ? WHERE idVenda_Restauracao = ?;";
+	const sqlEditarVendaRestauracao = "UPDATE venda_restauracao SET dataServico = ?, horaServico = ?, valorTotal = ?, ehVenda = ?, estaEntregue = ?, qtdeConsoles = ?, descricaoRestauracao = ?, avaliacao = avaliacao, FK_idEmpresa = ?, FK_idCliente = ?, FK_idConsole = ? WHERE idVenda_Restauracao = ?;";
 
 	let idCliente, idEmpresa
 
@@ -321,165 +323,187 @@ async function editarVendaRestauracao(dados, inputId) {
 				reject(err);
 			}
 
-			// Selecionar idCliente pelo cpfCliente
-			connection.query(sqlSelecionarIdCliente, [dados.inputCPF], function (err, resultCliente) {
+			// Seleciona os dados do serviço antigo
+			connection.query(sqlSelecionarDadosServicoAntigo, inputId, function (err, resultServicoAntigo) {
 				if (err) {
-					console.log("Erro ao selecionar o id do Cliente no banco de dados: ", err);
+					console.log("Erro ao selecionar os dados do serviço antigo no banco de dados: ", err);
 					reject(err);
 				}
 
-				if (resultCliente.length === 0) {
-					var data = { sucesso: false, mensagem: "Cliente não encontrado." };
-					var json = [data];
-					resolve(JSON.stringify(json));
-					connection.release();
-					return;
-				}
-				idCliente = resultCliente[0].idCliente;
+				const idEmpresaAntigo = resultServicoAntigo[0].FK_idEmpresa
+				const idConsoleAntigo = resultServicoAntigo[0].FK_idConsole
+				const qtdeConsolesAntigo = resultServicoAntigo[0].qtdeConsoles
 
-				// Selecionar idEmpresa pelo estadoEmpresa
-				connection.query(sqlSelecionarIdEmpresa, [dados.inputCompanyState], function (err, resultEmpresa) {
+				// Selecionar idCliente pelo cpfCliente
+				connection.query(sqlSelecionarIdCliente, [dados.inputCPF], function (err, resultCliente) {
 					if (err) {
-						console.log("Erro ao selecionar o id da Empresa no banco de dados: ", err);
+						console.log("Erro ao selecionar o id do Cliente no banco de dados: ", err);
 						reject(err);
 					}
 
-					if (resultEmpresa.length === 0) {
-						var data = { sucesso: false, mensagem: "Empresa não encontrada." };
+					if (resultCliente.length === 0) {
+						var data = { sucesso: false, mensagem: "Cliente não encontrado." };
 						var json = [data];
 						resolve(JSON.stringify(json));
 						connection.release();
 						return;
 					}
-					idEmpresa = resultEmpresa[0].idEmpresa;
 
-					if (dados.ehVenda) {
-						// Verificar a quantidade atual no estoque
-						connection.query(sqlSelecionarQuantAtual, [dados.inputId, idEmpresa], function (err, resultQuantAtual) {
-							if (err) {
-								console.log("Erro ao selecionar a quantidade atual do estoque no banco de dados: ", err);
-								reject(err);
-							}
+					idCliente = resultCliente[0].idCliente;
 
-							const quantAtual = resultQuantAtual.length > 0 ? resultQuantAtual[0].quantAtual : 0;
+					// Selecionar idEmpresa pelo estadoEmpresa
+					connection.query(sqlSelecionarIdEmpresa, [dados.inputCompanyState], function (err, resultEmpresa) {
+						if (err) {
+							console.log("Erro ao selecionar o id da Empresa no banco de dados: ", err);
+							reject(err);
+						}
 
-							if (quantAtual < dados.inputQuantity) {
-								var data = { sucesso: false, mensagem: "A empresa não possui quantidade de consoles suficiente para a venda!" };
-								var json = [data];
-								resolve(JSON.stringify(json));
-								connection.release();
-								return;
-							}
+						if (resultEmpresa.length === 0) {
+							var data = { sucesso: false, mensagem: "Empresa não encontrada." };
+							var json = [data];
+							resolve(JSON.stringify(json));
+							connection.release();
+							return;
+						}
+						idEmpresa = resultEmpresa[0].idEmpresa;
 
-							// Atualizar o estoque
-							connection.query(sqlAtualizarEstoque, [dados.inputQuantity, dados.inputId, idEmpresa], function (err, resultAtualizacaoEstoque) {
+						if (dados.inputEhVenda) {
+							// Verificar a quantidade atual no estoque
+							connection.query(sqlSelecionarQuantAtual, [dados.inputIdConsole, idEmpresa], function (err, resultQuantAtual) {
 								if (err) {
-									console.log("Erro ao atualizar o estoque no banco de dados: ", err);
-									reject(err);
-								}
-							});
-
-							if (quantAtual - dados.inputQuantity === 0) {
-								// Deletar estoque se quantidade atual for zero
-								connection.query(sqlDeletarEstoque, [dados.inputId, idEmpresa], function (err, resultDeletarEstoque) {
-									if (err) {
-										console.log("Erro ao deletar o estoque no banco de dados: ", err);
-										reject(err);
-									}
-								});
-							}
-						});
-					} else {
-						try {
-							// Cadastra um Console sem Estoque
-							connection.query(sqlEditarTabelaConsole, 
-								[
-									dados.inputModel, 
-									dados.inputProducer, 
-									dados.inputLaunchDate, 
-									dados.inputOriginality, 
-									dados.inputPrice, 
-									dados.inputConsoleDescription, 
-									dados.inputIdConsole
-								], function (err, resultEditarTabelaConsole) {
-								if (err) {
-									console.log("Erro ao editar na tabela Console no banco de dados: ", err);
+									console.log("Erro ao selecionar a quantidade atual do estoque no banco de dados: ", err);
 									reject(err);
 								}
 
-								// Insira os dados na tabela venda_restauracao para uma restauração
-								connection.query(sqlEditarVendaRestauracao, 
-									[
-										dados.inputServiceDate, 
-										dados.inputServiceHour, 
-										dados.inputTotalValue, 
-										false, 
-										dados.inputDelivery, 
-										dados.inputQuantity, 
-										dados.inputRestorationDescription, 
-										idEmpresa, 
-										idCliente, 
-										dados.inputIdConsole, 
-										inputId
-									], function (err, resultEditarVendaRestauracao) {
-									if (err) {
-										console.log("Erro ao editar a Venda/Restauração no banco de dados: ", err);
-										reject(err);
-									}
+								const quantAtual = resultQuantAtual.length > 0 ? resultQuantAtual[0].quantAtual : 0;
 
-									var data = { sucesso: true, mensagem: "Restauração atualizada com sucesso!" };
+								if (quantAtual < dados.inputQuantity) {
+									var data = { sucesso: false, mensagem: "A empresa não possui quantidade de consoles suficiente para a venda!" };
 									var json = [data];
 									resolve(JSON.stringify(json));
 									connection.release();
-								});
+									return;
+								}
+
+								// Arrumar o estoque antigo
+								connection.query(sqlArrumarEstoque, [qtdeConsolesAntigo, idConsoleAntigo, idEmpresaAntigo], function (err, resultAtualizarEstoqueAntigo) {
+									if (err) {
+										console.log("Erro ao atualizar o estoque antigo no banco de dados: ", err);
+										reject(err);
+									}
+
+									// Atualizar o estoque
+									connection.query(sqlAtualizarEstoque, [dados.inputQuantity, dados.inputIdConsole, idEmpresa], function (err, resultAtualizacaoEstoque) {
+										if (err) {
+											console.log("Erro ao atualizar o estoque no banco de dados: ", err);
+											reject(err);
+										}
+
+										if (quantAtual - dados.inputQuantity == 0) {
+											// Deletar estoque se quantidade atual for zero
+											connection.query(sqlDeletarEstoque, [dados.inputIdConsole, idEmpresa], function (err, resultDeletarEstoque) {
+												if (err) {
+													console.log("Erro ao deletar o estoque no banco de dados: ", err);
+													reject(err);
+												}
+											});
+										}
+
+										try {
+											// Atualiza os dados na tabela venda_restauracao para uma venda
+											connection.query(sqlEditarVendaRestauracao,
+												[
+													dados.inputServiceDate,
+													dados.inputServiceHour,
+													dados.inputTotalValue,
+													true,
+													dados.inputDelivery,
+													dados.inputQuantity,
+													dados.inputRestorationDescription,
+													idEmpresa,
+													idCliente,
+													dados.inputIdConsole,
+													inputId
+												], function (err, resultEditarVendaRestauracao) {
+													if (err) {
+														console.log("Erro ao editar a Venda/Restauração no banco de dados: ", err);
+														reject(err);
+													}
+
+													var data = { sucesso: true, mensagem: "Venda atualizada com sucesso!" };
+													var json = [data];
+													resolve(JSON.stringify(json));
+												});
+										} catch (error) {
+											var data = { sucesso: false, mensagem: "Erro ao atualizar uma Venda." };
+											var json = [data];
+											resolve(JSON.stringify(json));
+											connection.release();
+										}
+									});
+								})
 							});
-						} catch (error) {
-							var data = { sucesso: false, mensagem: "Erro ao atualizar uma Restauração." };
-							var json = [data];
-							resolve(JSON.stringify(json));
-							connection.release();
-						}
-					}
+						} else {
+							try {
+								// Atualiza um Console sem Estoque
+								connection.query(sqlEditarTabelaConsole,
+									[
+										dados.inputModel,
+										dados.inputProducer,
+										dados.inputLaunchDate,
+										dados.inputOriginality,
+										dados.inputPrice,
+										dados.inputConsoleDescription,
+										dados.inputIdConsole
+									], function (err, resultEditarTabelaConsole) {
+										if (err) {
+											console.log("Erro ao editar na tabela Console no banco de dados: ", err);
+											reject(err);
+										}
 
-					try {
-						// Insira os dados na tabela venda_restauracao para uma venda
-						connection.query(sqlEditarVendaRestauracao, 
-							[
-								dados.inputServiceDate, 
-								dados.inputServiceHour, 
-								dados.inputTotalValue, 
-								true, dados.inputDelivery, 
-								dados.inputQuantity, 
-								dados.inputRestorationDescription, 
-								idEmpresa, 
-								idCliente, 
-								dados.inputId, 
-								dados.inputIdVendaRestauracao
-							], function (err, resultEditarVendaRestauracao) {
-							if (err) {
-								console.log("Erro ao editar a Venda/Restauração no banco de dados: ", err);
-								reject(err);
+										// Insira os dados na tabela venda_restauracao para uma restauração
+										connection.query(sqlEditarVendaRestauracao,
+											[
+												dados.inputServiceDate,
+												dados.inputServiceHour,
+												dados.inputTotalValue,
+												false,
+												dados.inputDelivery,
+												dados.inputQuantity,
+												dados.inputRestorationDescription,
+												idEmpresa,
+												idCliente,
+												dados.inputIdConsole,
+												inputId
+											], function (err, resultEditarVendaRestauracao) {
+												if (err) {
+													console.log("Erro ao editar a Venda/Restauração no banco de dados: ", err);
+													reject(err);
+												}
+
+												var data = { sucesso: true, mensagem: "Restauração atualizada com sucesso!" };
+												var json = [data];
+												resolve(JSON.stringify(json));
+											});
+									});
+							} catch (error) {
+								var data = { sucesso: false, mensagem: "Erro ao atualizar uma Restauração." };
+								var json = [data];
+								resolve(JSON.stringify(json));
+								connection.release();
 							}
-
-							var data = { sucesso: true, mensagem: "Venda atualizada com sucesso!" };
-							var json = [data];
-							resolve(JSON.stringify(json));
-							connection.release();
-						});
-					} catch (error) {
-						var data = { sucesso: false, mensagem: "Erro ao atualizar uma Venda." };
-						var json = [data];
-						resolve(JSON.stringify(json));
-						connection.release();
-					}
+						}
+					});
 				});
-			});
+			})
+
+			connection.release();
 		});
 	});
 }
 
 async function verificarEntregaVendaRestauracao(inputId) {
-	
+
 	const sqlVerificarVenda = "SELECT CAST(estaEntregue AS DECIMAL) AS estaEntregue FROM venda_restauracao WHERE idVenda_Restauracao = ?;";
 
 	return new Promise(function (resolve, reject) {
@@ -496,7 +520,7 @@ async function verificarEntregaVendaRestauracao(inputId) {
 					reject(err);
 				}
 
-				if(resultVenda[0].estaEntregue == 1) {
+				if (resultVenda[0].estaEntregue == 1) {
 					resolve(true)
 				}
 				else {
